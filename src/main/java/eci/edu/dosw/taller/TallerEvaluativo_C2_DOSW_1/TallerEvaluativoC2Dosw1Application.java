2d08ec297f5e49b3275af6e5d65ae46a755df9a0
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-10-09 12:31:13 -0500

    Estructura de Carpetas

---
+++ src/main/java/eci/edu/dosw/taller/TallerEvaluativo_C2_DOSW_1/TallerEvaluativoC2Dosw1Application.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.taller.TallerEvaluativo_C2_DOSW_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativoC2Dosw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativoC2Dosw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/dosw/taller/TallerEvaluativo_C2_DOSW_1/TallerEvaluativoC2Dosw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.taller.TallerEvaluativo_C2_DOSW_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativoC2Dosw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

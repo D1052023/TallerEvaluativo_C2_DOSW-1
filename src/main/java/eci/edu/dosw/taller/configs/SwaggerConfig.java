6235c4d50c3cb1e236ce67e022bafb679430aa2a
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-10-10 11:24:08 -0500

    Configuracion básica de Swagger, Jacoco, sonarQube

---
+++ src/main/java/eci/edu/dosw/taller/configs/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package eci.edu.dosw.taller.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+/**
+ * Clase que configura la información que aparecerá en la documentación de la API.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Define un bean de tipo OpenAPI que sera utilizado por Springdoc OpenAPI
+     * para generar la documentación de la API automáticamente.
+     *
+     */
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MÁSTER CHEF CELEBRITY API")
+                        .description("API para la gestión de recetas de cocina")
+                        .version("1.0"));
+    }
+}
